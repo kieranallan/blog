9110f90d2c5789c28e1e3778b1a1a2f4dd6842bf
Kieran Allan <kieran@bookingexperts.nl>
2022-08-30 11:13:05 +0200

    added new accommodation feature at /accommodations/new

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -6,4 +6,23 @@ class AccommodationsController < ApplicationController
   def show
     @accommodation = Accommodation.find(params[:id])
   end
+
+  def new
+    @accommodation = Accommodation.new
+  end
+
+  def create
+    @accommodation = Accommodation.new(accommodation_params)
+
+    if @accommodation.save
+      redirect_to @accommodation
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def accommodation_params
+      params.require(:accommodation).permit(:title, :description, :persons)
+    end
 end
--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,2 +1,5 @@
 class Accommodation < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :persons, presence: true
 end
