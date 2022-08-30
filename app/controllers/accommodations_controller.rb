104730cc68316828a9ddae08c8d936f50106f82e
Kieran Allan <kieran@bookingexperts.nl>
2022-08-30 10:52:53 +0200

    link to individual accommodations on home page

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -2,4 +2,8 @@ class AccommodationsController < ApplicationController
   def index
     @accommodations = Accommodation.all
   end
+
+  def show
+    @accommodation = Accommodation.find(params[:id])
+  end
 end
